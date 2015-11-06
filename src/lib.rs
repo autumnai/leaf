a26e885ddfccd3f683be813911562032273751b9
MichaelHirn <michael.hirn@storeness.de>
2015-11-07 00:06:51 +0100

    docs(lib): add project philosophie

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-//! **Leaf is a open, fast and a well-designed, modular Framework for distributed Deep Learning on
-//! {C, G}PUs.**
+//! Leaf is a open, fast and a well-designed, modular Framework for distributed Deep Learning on
+//! {C, G}PUs.
 //!
 //! ## Overview
 //!
 //! To build a Deep Neural Network you create a [Network][network] which is a container for all
 //! different types of [Layers][layers]. These layers are grouped in different types such as
-//! [Activation Layers][activation], [Loss Layers][loss] which states the characteristics of the
+//! [Activation Layers][activation] and [Loss Layers][loss] which states the characteristics of the
 //! layer.
 //!
 //! Now to train your network you will use one of the [Solvers][solvers]. The Solver defines the
@@ -13,6 +13,21 @@
 //!
 //! The operations can run on different Backends {CPU, GPU} and must not be defined at compile
 //! time, which allows for easy backend swapping.
+//!
+//! ## Philosophie
+//!
+//! We are strong believers in the technology of Machine Learning. We put our experience in
+//! software engineering into Leaf, to solve our own need for a modern, performant and easy-to-use
+//! Deep Learning Framework. These principles direct our decisions on Leaf and related projects.
+//!
+//! * __Performance__: For research and industry speed and efficency are curcial for
+//! state-of-the-art machine learning over massive data and networks.
+//! * __Architecture__: Designing an open architecture that follows best practices and concepts in
+//! Engineering such as modularity, flexibility and expressiveness is critical to stimulate future
+//! innovation.
+//! * __Documentation__: A well-written documentation that addresses both concepts and
+//! implementations, empowers developers and researchers to contribute their unique experience to
+//! the project for the benefit of everyone.
 //!
 //! ## Examples
 //!
@@ -25,28 +40,25 @@
 //!
 //! ## Development
 //!
-//! The implementation of various Layers is pretty scarce at the moment.  
+//! The implementation of various Layers is pretty scarce at the moment.<br/>
 //! There are around a dozen layers, which are really important and would increase the value and
-//! functionality of Leaf tremendously.  
-//! Progress get tracked at
+//! functionality of Leaf tremendously.<br/>
+//! Progress get tracked at<br/>
+//!
 //! - [Issue #18 for Loss Layers][issue-loss]
 //! - [Issue #19 for Activation Layers][issue-activation]
 //! - [Issue #20 for Common Layers][issue-common]
 //!
-//! To better structure these different layer types and state their functionality and behavior more
-//! clearly, we will give them their own mod. [Issue #21][issue-mod]
-//!
 //! [network]: ./network/index.html
 //! [layers]: ./layers/index.html
-//! [activation]: #
-//! [loss]: #
+//! [activation]: ./layers/activation/index.html
+//! [loss]: ./layers/loss/index.html
 //! [solvers]: ./solvers/index.html
 //! [optimization]: https://en.wikipedia.org/wiki/Stochastic_optimization
 //!
 //! [issue-loss]: https://github.com/autumnai/leaf/issues/18
 //! [issue-activation]: https://github.com/autumnai/leaf/issues/19
 //! [issue-common]: https://github.com/autumnai/leaf/issues/20
-//! [issue-mod]: https://github.com/autumnai/leaf/issues/21
 #![feature(plugin)]
 #![feature(augmented_assignments)]
 #![plugin(clippy)]
